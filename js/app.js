2ce289c1e003ce8c86c0dafbd8649e459d2a6dc2
JeongMinSeung <91643097+JeongMinSeung@users.noreply.github.com>
2021-10-20 23:49:08 +0900

    2021.10.20
    ibm i2 solution 추가

--- js/app.js
+++ js/app.js
@@ -8,28 +8,6 @@ $(document).ready(function(){
     $('.title_wrap').addClass('on');
     $('.subtitle').addClass('on');
     $('.side_nav li:nth-child(1) a').addClass('active');
-
-// 사업분야 이미지 애니메이션
-
-// slidingBox('.a1',800,'url(../image/test.png)no-repeat',100);
-// slidingBox('.a2',500,'url(../image/test.png)no-repeat',700);
-// slidingBox('.a3',500,'url(../image/test.png)no-repeat',900);
-
-// function slidingBox(item, speed, color, delay){
-// 	$(item).append(
-// 		$('<div class="inner">').css({
-// 			'width':'100%','height':'100%',
-// 			'position':'absolute', 'top':'0px','left':'-100%',
-// 			'background':color,
-// 			'background-size':'cover'
-// 		}).delay(delay).animate({'left':'0%'},speed,'easeOutExpo')
-// 			// $(item).css({'color':color})
-// 			// $(this).animate({'left':'100%'},speed,'easeInExpo', function(){
-// 			// 	$(this).remove();
-// 			// })
-		
-// 	)
-// }
 })
 
 $(window).on('scroll',function(){
@@ -41,7 +19,6 @@ $(window).on('scroll',function(){
 	let solution = $('.a1').offset().top - $(window).height() + $('header').height();
 	let audit = $('.a2').offset().top - $(window).height() + $('header').height();
 	let forensic = $('.a3').offset().top - $(window).height() + $('header').height();
-	let ftr = $('footer').offset().top - $(window).height() + $('header').height();
 
 	$('.side_nav>ul>li>a').removeClass('active');
 	if(sct >= s1 && sct < s2){
@@ -192,39 +169,3 @@ $('.header_wrap ul li').on('click', function (e) {
 		scrollTop: target_pos
 	}, 700, 'swing');
 });
-//var posTop =(page-1) * $(window).height();
-//    mHtml.animate({scrollTop : posTop});
-/*
-		window.addEventListener("wheel", function(e){
-	    e.preventDefault();
-		},{passive : true}); 
- */
-
-// mHtml.animate({scrollTop : 0},10);
-// window.addEventListener("wheel", function(e){
-// 	if(test) e.preventDefault();
-// 		},{passive : false})
-		
-// $(window).on("wheel", function(e) {
-// 	//마우스 스크롤시 값이 +1(휠다운) / -1(휠업)
-// 	var scroll = e.originalEvent.deltaY;
-//     if(mHtml.is(":animated")) return;
-// 	//스크롤Down
-//     if(scroll > 0 && $(window).scrollTop()<$(window).height()) {
-// 		scrollevent=true;
-// 		mHtml.animate({scrollTop : $(window).height()});
-// 		$('.title_wrap').removeClass('on');
-// 		$('.subtitle').removeClass('on');
-		
-// 	//스크롤Up
-// 	}else if(scroll < 0 && $(window).scrollTop()<=$(window).height()) {
-// 		scrollevent=true;
-//      	mHtml.animate({scrollTop : 0});
-// 		$('.title_wrap').addClass('on');
-// 		$('.subtitle').addClass('on');
-// 	//이후 나머지 스크롤 허용
-//     }else{
-// 		scrollevent = false;
-// 	}
-	
-// });
